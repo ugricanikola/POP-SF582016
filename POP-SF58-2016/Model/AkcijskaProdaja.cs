c12a1f3359c6a70ef3177bb2934815da469ad12d
ugricanikola@gmail.com <ugricanikola@gmail.com>
2017-10-16 18:12:48 +0200

    test

--- POP-SF58-2016/Model/AkcijskaProdaja.cs
+++ POP-SF58-2016/Model/AkcijskaProdaja.cs
@@ -11,7 +11,6 @@ namespace POP_SF58_2016.Model
         public int ID { get; set; }
         public DateTime DatumPocetkaAkcije { get; set; }
         public DateTime DatumZavrsetkaAkcije { get; set; }
-        
         public bool Obrisan { get; set; }
     }
 }
